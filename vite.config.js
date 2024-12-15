31a35472fba434ad98856138b97ed4e6624a286a
yashmahajan01082003 <yashmahajan01082003@gmail.com>
2024-12-16 00:21:23 +0530

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is correct based on your deployment
+
 })
